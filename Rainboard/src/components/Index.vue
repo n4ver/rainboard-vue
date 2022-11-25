0d5e4023a8ed19403ef60126a7380cdcf5be1448
n4ver <nicholaslkchng@gmail.com>
2022-11-25 16:36:17 +0800

    Change things

--- Rainboard/src/components/Index.vue
+++ Rainboard/src/components/Index.vue
@@ -2,17 +2,13 @@
 </script>
 
 <template>
-    <Head>
-        <title>Rain Dashboard</title>
-        <link rel="stylesheet" href=".././style.css"/>
-    </Head>
-        <main class="mb-auto">
-            <div class="p-4 mx-auto max-w-screen-md">
-                <p class="my-6">
-                    THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG
-                </p>
-            </div>
-        </main>
+    <main class="mb-auto">
+        <div class="p-4 mx-auto max-w-screen-md">
+            <p class="my-6">
+                THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG
+            </p>
+        </div>
+    </main>
 </template>
 
 <style scoped>
