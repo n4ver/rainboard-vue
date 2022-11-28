588055bab17d39167b30dd8ff50a6d4d71a1bc82
n4ver <nicholaslkchng@gmail.com>
2022-11-28 13:28:56 +0800

    Header changes

--- Rainboard/src/components/Header.vue
+++ Rainboard/src/components/Header.vue
@@ -2,14 +2,49 @@
 export default {
   data(){
     return {
-      search : null
+        error : "", // TODO: use error
+        search : ""
     }
   },
   methods: {
-    submit(){
-      //if you want to send any data into server before redirection then you can do it here
-      console.log(this.search);
-      this.$router.push("/log/" + this.search);
+    submit() {
+        //if you want to send any data into server before redirection then you can do it here
+        console.log(this.search);
+        let log = ""
+        const regex1 = /^(https:\/\/logs\.tf\/)(\d+)(#\d+)$/
+        const regex2 = /^(https:\/\/logs\.tf\/)(\d+)(#)$/
+        const regex3 = /^(https:\/\/logs\.tf\/)(\d+)$/
+        if (regex1.test(this.search)) {
+            log = regex1.exec(this.search)![2];
+        }
+        if (regex2.test(this.search)) {
+            log = regex2.exec(this.search)![2];
+        }
+        if (regex3.test(this.search)) {
+            log = regex3.exec(this.search)![2];
+        }
+
+      this.$router.push("/log/" + log);
+    },
+    validate() {
+        const regex1 = /^(https:\/\/logs\.tf\/)(\d+)(#\d+)$/
+        const regex2 = /^(https:\/\/logs\.tf\/)(\d+)(#)$/
+        const regex3 = /^(https:\/\/logs\.tf\/)(\d+)$/
+
+        this.error = "";
+        let url = this.search;
+        if (!url) {
+            this.error.concat("Enter Log ID");
+            return false;
+        }
+        else {
+            if (!(regex1.test(url) || regex2.test(url) || regex3.test(url))) {
+                this.error.concat("Invalid Log");
+                return false;
+            }
+            return true;
+        }
+
     }
   }
 }
@@ -20,16 +55,17 @@ export default {
             <div id="head" class="justify-center">
                 <div id="head-wrapper" class="flex-none h-full w-full flex-nowrap">
                     <div id="nav-bar" class="flex items-center bg-gradient-to-r from-indigo-500 gap-4 h-16 px-1 uppercase">
-                        <div id="search-wrapper" class="flex px-4">
-                            <form id="search-bar" class="flex">
-                                <div class="flex flex-row flex-nowrap bg-indigo-900 rounded-full min-h-full pl-2">
+                        <div id="search-wrapper" class="flex px-4 w-80">
+                            <form id="search-bar" class="flex w-full" @submit="validate">
+                                <div class="flex flex-row flex-nowrap bg-indigo-900 rounded-full min-h-full pl-2 w-full">
                                     <span class="flex flex-row flex-nowrap gap-2 items-center justify-center">
-                                        <svg viewBox="0 0 24 24" class="h-5 w-5">
-                                            <path fill="white" d="M23.785 21.937L18.12 16.27a.554.554 0 0 0-.397-.163h-.616a9.683 9.683 0 0 0 2.362-6.348c0-5.363-4.346-9.709-9.71-9.709S.05 4.396.05 9.759s4.346 9.709 9.709 9.709a9.683 9.683 0 0 0 6.348-2.362v.616c0 .15.06.29.163.397l5.667 5.666c.22.22.574.22.794 0l1.054-1.054a.56.56 0 0 0 0-.794zM9.76 17.227c-4.126 0-7.468-3.342-7.468-7.468S5.633 2.29 9.759 2.29s7.468 3.342 7.468 7.468-3.342 7.468-7.468 7.468z"></path>
-                                        </svg>
+                                        <button type="submit" @click.stop.prevent="submit()">
+                                            <svg viewBox="0 0 24 24" class="h-5 w-5">
+                                                <path fill="white" d="M23.785 21.937L18.12 16.27a.554.554 0 0 0-.397-.163h-.616a9.683 9.683 0 0 0 2.362-6.348c0-5.363-4.346-9.709-9.71-9.709S.05 4.396.05 9.759s4.346 9.709 9.709 9.709a9.683 9.683 0 0 0 6.348-2.362v.616c0 .15.06.29.163.397l5.667 5.666c.22.22.574.22.794 0l1.054-1.054a.56.56 0 0 0 0-.794zM9.76 17.227c-4.126 0-7.468-3.342-7.468-7.468S5.633 2.29 9.759 2.29s7.468 3.342 7.468 7.468-3.342 7.468-7.468 7.468z"></path>
+                                            </svg>
+                                        </button>
                                     </span>
-                                    <input type="text" v-model="search" id="search" style="background: none" required placeholder="Search..." class=" border-none outline-none text-white px-2 text-lg"/>
-                                    <button type="submit" @click.stop.prevent="submit()" style="visibility: hidden">Submit</button>
+                                    <input type="text" v-model="search" id="search" style="background: none" required placeholder="Submit logs.tf URL..." class=" border-none outline-none text-white px-2 text-lg w-full"/>
                                 </div>
                             </form>
                         </div>
