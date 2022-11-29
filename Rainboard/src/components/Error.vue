35a0131eb5671e32485575f3b224a1e57b32af81
n4ver <nicholaslkchng@gmail.com>
2022-11-29 16:53:20 +0800

    Compartmentalize error pages

---
+++ Rainboard/src/components/Error.vue
@@ -0,0 +1,28 @@
+<script lang="ts">
+export default {
+    data() {
+        return {
+            errorCode: "",
+            errorMessage: ""
+        }
+    }
+}
+</script>
+
+<template>
+    <main>
+        <section class="w-full flex items-center justify-center">
+            <div class="text-center">
+                <h1 class="text-6xl md:text-9xl font-medium">
+                    {{ errorCode }}
+                </h1>
+                <div class="py-4 text-2xl md:text-3xl">
+                    {{ errorMessage }}
+                </div>
+                <div class="py-4">
+                    <a href="/" class="hover:underline">Back to the Homepage</a>
+                </div>
+            </div>
+        </section>
+      </main>
+</template>
