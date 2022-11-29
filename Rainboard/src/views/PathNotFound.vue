d2989e184b25f83f6522012d310605dafa9cb76d
n4ver <nicholaslkchng@gmail.com>
2022-11-29 16:53:42 +0800

    Yeah

--- Rainboard/src/views/PathNotFound.vue
+++ Rainboard/src/views/PathNotFound.vue
@@ -1,26 +1,24 @@
 <script setup lang="ts">
 import Header from "../components/Header.vue"
 import Footer from "../components/Footer.vue"
+import Error from "../components/Error.vue"
+</script>
+
+<script lang="ts">
+export default {
+    data() {
+        return {
+            errorCode: "404",
+            errorMessage: "We couldn't find the page you're looking for."
+        }
+    }
+}
 </script>
 
 <template>
     <Header />
-      <main>
-        <section class="w-full flex items-center justify-center">
-            <div class="text-center">
-                <h1 class="text-6xl md:text-9xl font-medium">
-                    404
-                </h1>
-                <div class="py-4 text-2xl md:text-3xl">
-                    We couldn't find the page you're looking for.
-                </div>
-                <div class="py-4">
-                    <a href="/" class="hover:underline">Back to the Homepage</a>
-                </div>
-            </div>
-        </section>
-      </main>
-      <Footer />
+    <Error />
+    <Footer />
 </template>
 
 <style scoped>
